e49d9f7e560e074b11744c374456fcf2ffed4421
AliciaT1402 <alicia.tiopan@example.com>
2025-03-16 23:22:32 +0800

    Added function to chatbot management

--- backend/services/facade/chatbot_service.py
+++ backend/services/facade/chatbot_service.py
@@ -9,19 +9,36 @@ class ChatbotService(ABC):
 
     @abstractmethod
     def create_chatbot(self, user_id: str, data: dict) -> tuple:
-        pass
+        data["user_id"] = user_id
+        response = self.supabase.from_("chatbots").select("*").eq("user_id", user_id).execute()
+        
+        if response.data:
+            return True
+        return False
 
     @abstractmethod
     def get_chatbot_details(self, user_id: str, chatbot_id: str) -> tuple:
-        pass
+        response = self.supabase.from_("chatbots").select("*").eq("user_id", user_id).eq("id", chatbot_id).execute()
+        
+        if response.data:
+            return True
+        return False
 
     @abstractmethod
     def update_chatbot(self, user_id: str, chatbot_id: str, data: dict) -> tuple:
-        pass
+        response = self.supabase.from_("chatbots").update(data).eq("user_id", user_id).eq("id", chatbot_id).execute()
+
+        if response.data:
+            return True
+        return False
 
     @abstractmethod
     def delete_chatbot(self, user_id: str, chatbot_id: str) -> tuple:
-        pass
+        response = self.supabase.from_("chatbots").update(data).eq("user_id", user_id).eq("id", chatbot_id).execute()
+        
+        if response.data:
+            return True
+        return False
 
     # 2. Manage Documents
     @abstractmethod
