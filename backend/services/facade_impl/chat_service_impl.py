1010a297f020a4c7cfcddd2a8acce795dfb2ca6f
shijie.gan <shijiegan.gs@gmail.com>
2025-04-11 10:10:26 +0800

    chat ordering

--- backend/services/facade_impl/chat_service_impl.py
+++ backend/services/facade_impl/chat_service_impl.py
@@ -352,6 +352,7 @@ class ChatServiceImpl(ChatService):
                 .select('message') \
                 .eq('chatbot_id', chatbot_id) \
                 .eq('session_id', session_id) \
+                .order('created_at') \
                 .execute()
 
             if not result.data:
